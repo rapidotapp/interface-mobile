4feaa93ee432d9ac2146b475e90caa06d808e5a4
Caleb Denio <cjdenio44@gmail.com>
2020-08-23 14:17:31 -0400

    Create class

---
+++ lib/widgets/switch.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/widgets.dart';
+
+class RapidSwitch extends StatefulWidget {
+  final List<String> items;
+  final int initialSelected;
+
+  RapidSwitch({this.items = const [], this.initialSelected = 0}) : assert(items.length >= 1);
+
+  RapidSwitchState createState() => RapidSwitchState();
+}
+
+class RapidSwitchState extends State<RapidSwitch> {
+  Widget build(BuildContext ctx) {
+    return Container();
+  }
+}
